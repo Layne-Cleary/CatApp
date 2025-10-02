8893159e05c168f90602f22ed177b8f919c96185
Layne Cleary <0cleal94@solent.ac.uk>
2025-10-02 14:51:03 +0100

    Modified walk method.

--- src/Cat.kt
+++ src/Cat.kt
@@ -1,14 +1,26 @@
-class Cat (val name: String, var age: Int, var weight: Int) {
+class Cat (private val name: String, private var age: Int, weightIn: Int) {
 
-    fun walk() {
-        weight--
+    var weight = 0
+        set(newWeight) {
+            if(newWeight >= 5) {
+                field = newWeight
+            }
+        }
+
+    init {
+        weight = weightIn
+    }
+
+    public fun walk(distance: Int) : Boolean
+    {
+        if(weight - distance >= 5) {
+            weight -= distance
+            return true
+        }
+        return false
     }
 
     override fun toString() : String {
-        return "Name: $name Age: $age"
-    }
-
-    fun eat() {
-        weight++
+        return "Name: $name, Age: $age, Weight: $weight"
     }
 }
--- src/main.kt
+++ src/main.kt
@@ -1,8 +1,6 @@
 fun main() {
     val binnie = Cat("Binnie", 10, 10)
     val clyde = Cat("Clyde", 5, 5)
-    binnie.walk()
-    clyde.walk()
-    println(binnie)
-    println(clyde)
+
+
 }
